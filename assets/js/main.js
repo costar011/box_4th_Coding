387f489c51d802ee9d232cd1cf9dc071ddd2237b
YERIM <dnaer456@gmail.com>
2021-03-13 14:18:34 +0900

    error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,32 @@
-const color = {
+const box = document.getElementById("box-js");
+const colors = {
   black: "#000",
   red: "#e74c3c",
   blue: "#4a69bd",
-  hotpink: "#fd79a8",
+  hotPink: "#fd79a8",
 };
 
-// function 함수이름( [파라미터] ){
-//      한수실행코드
+// !함수의 선언!
+// function 함수이름([파라미터]){
+//       함수 실행 코드...
 // }
-// [] : 있어도 되고 없어도 된다.
 
 function changeBlackColor() {
-  const box = document.getElementById("box-js");
   box.style.backgroundColor = colors.black;
 }
 
 function changeRedColor() {
-  alert("red으로 바뀌어라");
+  box.style.backgroundColor = colors.red;
 }
 
 function changeBlueColor() {
-  alert("blue으로 바뀌어라");
+  box.style.backgroundColor = colors.blue;
 }
 
-function changeHotpinkColor() {
-  alert("hotpink으로 바뀌어라");
+function changeHotPinkColor() {
+  box.style.backgroundColor = colors.hotPink;
+}
+
+function randomColor() {
+  alert("뭐여?");
 }
