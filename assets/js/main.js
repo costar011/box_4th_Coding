50383d26b11f9e568c22ccf879a8a757c6324e81
YERIM <dnaer456@gmail.com>
2021-03-19 23:04:41 +0900

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,49 @@
 const box = document.getElementById("box-js");
 const colors = {
   black: "#000",
-  red: "#e74c3c",
+  red: "#e73c3c",
   blue: "#4a69bd",
-  hotPink: "#fd79a8",
+  hotpink: "#fd79a8",
+  green: "green",
 };
 
 // !함수의 선언!
 // function 함수이름([파라미터]){
-//       함수 실행 코드...
+//  함수 실행 코드...
 // }
 
-function changeBlackColor() {
+function changeColor1() {
   box.style.backgroundColor = colors.black;
 }
-
-function changeRedColor() {
+function changeColor2() {
   box.style.backgroundColor = colors.red;
 }
-
-function changeBlueColor() {
+function changeColor3() {
   box.style.backgroundColor = colors.blue;
 }
+function changeColor4() {
+  box.style.backgroundColor = colors.hotpink;
+}
+function randomColor() {
+  const colors = [
+    "black",
+    "red",
+    "blue",
+    "yellow",
+    "orange",
+    "grey",
+    "purple",
+    "skyblue",
+    "green",
+    "indigo",
+  ];
 
-function changeHotPinkColor() {
-  box.style.backgroundColor = colors.hotPink;
+  console.log(colors.lenfth);
+
+  const randomNumber = Math.random() * 10;
+  const resurt = Math.floor(randomNumber);
+
+  console.log(resurt);
+
+  box.style.backgroundColor = colors[resurt];
 }
